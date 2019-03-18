3a6ac8a9392d01de19289c602000c05cc4c947bf
Juliano Atanazio <juliano777@gmail.com>
2019-03-18 16:12:21 -0300

    20190318_1612

--- tutorial/01_hadoop.sh
+++ tutorial/01_hadoop.sh
@@ -45,7 +45,10 @@ export HADOOP_HDFS_HOME="\${HADOOP_HOME}"
 export YARN_HOME="\${HADOOP_HOME}"
 export HADOOP_COMMON_LIB_NATIVE_DIR="\${HADOOP_HOME}/lib/native"
 export LD_LIBRARY_PATH="\${LD_LIBRARY_PATH}:\${HADOOP_COMMON_LIB_NATIVE_DIR}"
-export HADOOP_OPTS="-Djava.library.path=\${HADOOP_COMMON_LIB_NATIVE_DIR} -XX:-PrintWarnings -Djava.net.preferIPv4Stack=true"
+export HADOOP_OPTS="-Djava.library.path=\${HADOOP_COMMON_LIB_NATIVE_DIR} \
+                    -XX:-PrintWarnings \
+                    -Djava.net.preferIPv4Stack=true \
+                    -Dhadoop.root.logger=WARN"
 export HADOOP_CONF_DIR="/etc/hadoop"                                   
 export HADOOP_LOG_DIR='/var/log/hadoop'
 export HADOOP_TMP_DIR='/var/local/hadoop/tmp'
